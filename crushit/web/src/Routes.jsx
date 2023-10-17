94128b8a4b61c1a06b25eca6aac7931e7cb63d91
tanner666 <tjg5@njit.edu>
2023-10-17 19:39:43 -0400

    added login and home page

--- crushit/web/src/Routes.jsx
+++ crushit/web/src/Routes.jsx
@@ -12,6 +12,8 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/home" page={HomePage} name="home" />
+      <Route path="/" page={LoginPage} name="login" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
