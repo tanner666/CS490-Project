7e3f5ee87fd9687463cbbfaf56e699c147fafa25
jrm947 <jrm94@njit.edu>
2023-10-29 09:47:03 -0400

    final

--- crushit/api/src/Routes.js
+++
@@ -1,6 +0,0 @@
-// api/src/Routes.js
-import { defineRoute } from '@redwoodjs/router'
-
-export const routes = () => {
-  defineRoute('userRegistration', 'api/src/functions/userRegistration', ['POST'])
-}
