9b8699d6ae58429acd89586af3272f7497ccec49
Bmogul <98663272+Bmogul@users.noreply.github.com>
2023-10-29 23:35:25 -0400

    tests for createUser

--- crushit/api/src/services/users/users.test.js
+++ crushit/api/src/services/users/users.test.js
@@ -1,74 +1,20 @@
 import { standard } from './users.scenarios'
 import {createUser, deleteUser, users, user,} from './users'
-import * as firebaseAuth from 'firebase/auth';
-import { getApps, initializeApp } from 'firebase/app'
-import { getAuth, createUserWithEmailAndPassword, deleteUser as dUser ,signInWithEmailAndPassword} from 'firebase/auth';
 
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-}
-const firebaseApp = initializeApp(firebaseConfig)
-const auth = firebaseAuth.getAuth(firebaseApp);
-
-let email = standard.user.one.email;
-let password = standard.user.one.password;
 
 describe('createUser', () => {
-  it('Creates a user', async () => {
-    const input = {email, password}
-    const context = {firebaseApp}
-    const createdUser = await (await createUser({input, context})).userDB
+  it('should create a user', async () => {
+    const input = { email: 'test@example.com', firebaseUid: '123456' };
+    const userDB = await createUser({ input });
+    expect(userDB).toBeDefined();
+    expect(userDB.email).toBe('test@example.com');
+    expect(userDB.firebaseUid).toBe('123456');
+  });
 
-    // console.log(createdUser)
-    expect(createdUser).toBeDefined();
-    expect(createdUser.email).toBe(email)
-    // console.log(createdUser.firebaseUid)
-    // console.log('test', auth)
-    let temp = {firebaseUid: createdUser.firebaseUid, auth}
-    await deleteUser(temp).then((res)=>{console.log(res)})
-  })
-
-  it('Creating user that already exists', async () =>{
-    const input = {email, password}
-    const context = {firebaseApp}
-    const mainUser = (await createUserWithEmailAndPassword(auth, email, password)).user
-
-    const createdUser = await (await createUser({input, context}))
-
-    expect(createdUser.success).toBe(false)
-    await deleteUser({firebaseUid: mainUser.uid, auth})
-  })
-})
-
-// describe('deleteUser', () => {
-//   it('deletes a user', async () => {
-//     await createUserWithEmailAndPassword(auth, email, password) 
-//     let user = (await signInWithEmailAndPassword(auth,email,password)).user
-//     const deletedUser = await deleteUser({ firebaseUid: user.uid, auth })
-
-//     expect(deletedUser).toBeDefined()
-//     expect(deletedUser.success).toBe(true)
-
-//   })
-// })
-
-describe('users', () => {
-  it('get list of users after creating a user', async()=>{
-    const input = {email, password}
-    const context = {firebaseApp}
-    const createdUser = await (await createUser({input, context})).userDB
-    
-    await users().then((users)=>{
-
-      console.log('users', users)
-      expect(users).toBeDefined()
-      expect(users.length).toBeGreaterThan(0)
-
-      
-
-    })
-    await deleteUser({firebaseUid: createdUser.firebaseUid, auth})
-  })
-})
+  it('should throw an error if user creation fails', async () => {
+    const input = { email: 'test@example.com', firebaseUid: '123456' };
+    jest.spyOn(db.user, 'create').mockRejectedValue(new Error('Failed to create user'));
+    await expect(createUser({ input })).rejects.toThrow('Failed to create user');
+  });
+});
