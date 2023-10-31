a05caf17eb361cbf00591ffe4cc0aa9c3342fd31
tanner666 <tjg5@njit.edu>
2023-10-31 04:02:04 -0400

    more front end styling for settings page

--- crushit/web/src/components/SettingsForm/SettingsForm.jsx
+++ crushit/web/src/components/SettingsForm/SettingsForm.jsx
@@ -66,31 +66,58 @@ export const SettingsForm = () => {
             {/*Rest of page */}
             <div className="w-full">
                 {/*Profile Header */}
-                <div className={`p-2 w-full mx-auto shadow-md ${theme === 'dark' ? 'bg-gray-700' : 'bg-white'}`}>
-                    <h2 className={`text-2xl font-dm font-bold mt-4 mb-2 ml-[3.4%] ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Profile</h2>
+                <div className={`pt-1 pb-1 w-full mx-auto shadow-md ${theme === 'dark' ? 'bg-gray-700' : 'bg-white'}`}>
+                    <h2 className={`text-2xl font-dm font-bold mt-2 mb-2 ml-[3%] ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Profile</h2>
                 </div>
-                <div className="forms ml-[4%] mr-[4%]">
+                <div className="forms ml-[3%] mr-[3%]">
                     <div className="flex justify-between items-center">
-                        <h2 className={`text-xl font-dm font-semibold mt-4 mb-2 ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>User Info</h2>
+                        <h2 className={`text-xl font-dm font-semibold mt-6 mb-2 ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>User Info</h2>
                         <ThemeToggle/>
                     </div>
-                    <div className={`p-8 w-full mx-auto rounded-lg shadow-md ${theme === 'dark' ? 'bg-gray-700' : 'bg-white'}`}>
+                    <div className={`pb-5 px-8 w-full mx-auto rounded-lg shadow-md ${theme === 'dark' ? 'bg-gray-700' : 'bg-white'}`}>
                         <div className="grid grid-cols-2 gap-8">
                             <NameField theme={theme}/>
                         </div>
                     </div>
-                    <h2 className={`text-xl font-dm font-semibold mt-4 mb-2 ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Change Password</h2>
-                    <div className={`p-8 w-full mx-auto rounded-lg shadow-md ${theme === 'dark' ? 'bg-gray-700' : 'bg-white'}`}>
+                    <h2 className={`text-xl font-dm font-semibold mt-6 mb-2 ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Change Password</h2>
+                    <div className={`pb-5 px-8 w-full mx-auto rounded-lg shadow-md ${theme === 'dark' ? 'bg-gray-700' : 'bg-white'}`}>
                         <div className="grid grid-cols-3 gap-8">
                             <PasswordField theme={theme}/>
                         </div>
                     </div>
-                    <h2 className={`text-xl font-dm font-semibold mt-4 mb-2 ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Podomoro Timer (Minutes)</h2>
-                    <div className={`p-8 w-full mx-auto rounded-lg shadow-md ${theme === 'dark' ? 'bg-gray-700' : 'bg-white'}`}>
+                    <h2 className={`text-xl font-dm font-semibold mt-6 mb-2 ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Podomoro Timer (Minutes)</h2>
+                    <div className={`pb-5 px-8 w-full mx-auto rounded-lg shadow-md ${theme === 'dark' ? 'bg-gray-700' : 'bg-white'}`}>
                         <div className="grid grid-cols-3 gap-8">
                             <TimerField theme={theme}/>
                         </div>
                     </div>
+                </div>
+                {/*Save Buttons*/}
+                <div className="pt-20 flex items-center justify-center space-x-4">
+                    <button
+                    type="button"
+                    className="w-[18%] py-3 rounded-lg mb-6 transition duration-150 border-2"
+                    style={{
+                        background: 'white',
+                        color: '#6284FF',
+                        borderColor: '#6284FF',
+                        boxShadow: '0px 4px 80px 0px rgba(98, 132, 255, 0.20)'
+                    }}
+                    //onClick={handleSignUp}
+                    >
+                    Cancel
+                    </button>
+                    <button
+                    type="button"
+                    className="text-white w-[18%] py-3 rounded-lg mb-6 transition duration-150"
+                    style={{
+                        background: 'linear-gradient(180deg, #6284FF 0%, #4B6DE9 100%)',
+                        boxShadow: '0px 4px 80px 0px rgba(98, 132, 255, 0.20)'
+                    }}
+                    //onClick={handleSignUp}
+                    >
+                    Save
+                    </button>
                 </div>
             </div>
         </div>
