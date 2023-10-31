86551c3082ce28d20e4911fd49ddbe01152c5bb1
tanner666 <tjg5@njit.edu>
2023-10-31 00:19:43 -0400

    updated setting page

--- crushit/web/src/components/SettingsForm/SettingsForm.jsx
+++ crushit/web/src/components/SettingsForm/SettingsForm.jsx
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const SettingsForm = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const toggleTheme = () => {
+        const updatedMode = !isDarkMode;
+        setIsDarkMode(updatedMode);
+    
+        if (updatedMode) {
+          document.body.classList.add('dark-mode');
+        } else {
+          document.body.classList.remove('dark-mode');
+        }
+      };
+    
+    const handleFirstNameChange = (event) => {
+        setFirstName(event.target.value);
+    };
+    
+    const handleLastNameChange = (event) => {
+        setLastName(event.target.value);
+    };
+
     return (
-        <div className="bg-white p-8 w-full max-w-5xl mx-auto rounded-lg shadow-md">
+        <div className={`button-container ${isDarkMode ? 'dark-mode' : ''}`}>
+          <div className="theme-text">
+            {isDarkMode ? 'Dark Theme' : 'Light Theme'}
+          </div>
+          <div className="toggle" onClick={toggleTheme}></div>
+    
+          {/* User Information */}
+          <div className="user-info" style={{ marginTop: '100px' }}>
+            <h2>User Info</h2>
+            <div style={{ display: 'flex', gap: '10px' }}>
+              <div>
+                <label htmlFor="firstName">First Name:</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  value={firstName}
+                  onChange={handleFirstNameChange}
+                  style={{ width: '800px' }} // Increase width for the first name input
+                />
+              </div>
+              <div>
+                <label htmlFor="lastName">Last Name:</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={lastName}
+                  onChange={handleLastNameChange}
+                  style={{ width: '800px' }} // Increase width for the last name input
+                />
+              </div>
+            </div>
+          </div>
+          <div className="bg-white p-8 w-full max-w-5xl mx-auto rounded-lg shadow-md">
             <h2 className="text-xl font-semibold mb-8">Change Password</h2>
             <div className="grid grid-cols-3 gap-8">
                 <PasswordField label="Current Password" />
@@ -10,7 +67,8 @@ export const SettingsForm = () => {
                 <PasswordField label="Confirm New Password" />
             </div>
         </div>
-    );
+        </div>
+      );
 }
 
 const PasswordField = ({ label }) => {
