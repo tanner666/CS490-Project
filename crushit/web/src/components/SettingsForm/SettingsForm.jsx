b5dfb8c8da7e4de23ff54f2542e1e2010e4a8e01
KaanIsmet <kaanismet7@gmail.com>
2023-10-31 00:43:02 -0400

    centered the two buttons

--- crushit/web/src/components/SettingsForm/SettingsForm.jsx
+++ crushit/web/src/components/SettingsForm/SettingsForm.jsx
@@ -26,20 +26,22 @@ export const SettingsForm = () => {
       <UserInfo />
       <ChangePassword newPassword={newPassword} onNewPasswordChange={handleNewPasswordChange} />
       <PomodoroTimer />
-      <button
-        className="bg-gray-300 text-gray-700 px-4 py-2 my-5 rounded-md hover:bg-gray-400"
-        onClick={() => {
-          // Handle Cancel button click event
-        }}
-      >
-        Cancel
-      </button>
-      <button
-        className="bg-blue-500 text-white px-4 py-2 my-5 rounded-md hover-bg-blue-700"
-        onClick={handleSaveClick}
-      >
-        Save
-      </button>
+      <div className="flex justify-center space-x-4"> {/* Center and add spacing between the buttons */}
+        <button
+          className="bg-gray-300 text-gray-700 px-4 py-2 my-5 mx-2 rounded-md hover:bg-gray-400"
+          onClick={() => {
+            // Handle Cancel button click event
+          }}
+        >
+          Cancel
+        </button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 my-5 mx-2 rounded-md hover-bg-blue-700"
+          onClick={handleSaveClick}
+        >
+          Save
+        </button>
+      </div>
     </div>
   );
 };
