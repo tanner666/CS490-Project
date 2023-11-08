969b5bd028122f1283ec362c23853a1a2af354f5
neselus <nlu2@njit.edu>
2023-11-08 12:45:22 -0500

    added icon images for name, password, and timer fields

--- crushit/web/src/components/SettingsField/SettingsField.jsx
+++ crushit/web/src/components/SettingsField/SettingsField.jsx
@@ -1,21 +1,24 @@
-import {useTheme} from '../ThemeContext/ThemeContext'
+import { useTheme } from '../ThemeContext/ThemeContext';
+import React from 'react';
 
+const SettingsField = ({ label, value, onChange, type = 'text', id, placeholder }) => {
+  const { theme } = useTheme();
 
-const SettingsField = ({label, value, onChange, type='text', id}) => {
-  const {theme} = useTheme();
   return (
     <div className="relative">
-        <label htmlFor={id} className={`block mb-2 ${theme === 'dark' ? 'text-white' : 'text-gray-600'}`}>{label}</label>
-        <input 
-          id={id}
-          type={type}
-          className={`border-2 p-2 rounded-lg w-full focus:border-blue-500 ${theme === 'dark' ? 'text-black' : 'text-almost-black'}`}
-          value={value}
-          onChange={onChange}
-          placeholder={label}
-        />
+      <label htmlFor={id} className={`block mb-2 ${theme === 'dark' ? 'text-white' : 'text-gray-600'}`}>
+        {label}
+      </label>
+      <input
+        id={id}
+        type={type}
+        className={`border-2 p-2 rounded-lg w-full focus:border-blue-500 ${theme === 'dark' ? 'text-black' : 'text-almost-black'}`}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SettingsField
+export default SettingsField;
--- crushit/web/src/components/TimerField/TimerField.jsx
+++ crushit/web/src/components/TimerField/TimerField.jsx
@@ -1,39 +1,56 @@
-import SettingsField from "../SettingsField/SettingsField"
-import React, {useState} from 'react'
+import SettingsField from "../SettingsField/SettingsField";
+import React from 'react';
 
+const TimerField = ({ podomoro, shortBreak, longBreak, handlePodomoroChange, handleShortBreakChange, handleLongBreakChange, theme }) => {
+  const timerIconUrl = 'https://drive.google.com/uc?id=1zVrNKtABhvWbYYbZkVL0zWngMhVaioVf';
 
-const TimerField = ({podomoro, shortBreak, longBreak, handlePodomoroChange, handleShortBreakChange, handleLongBreakChange, theme}) => {
   return (
     <>
       <SettingsField
         id="Podomoro"
-        label="Podomoro"
+        label={
+          <div className="flex items-center">
+            <img src={timerIconUrl} alt="Timer" className="mr-2 h-5 w-5" />
+            Pomodoro
+          </div>
+        }
         value={podomoro}
         onChange={handlePodomoroChange}
         theme={theme}
         type="number"
+        placeholder="Enter Pomodoro duration"
       />
       <SettingsField
         id="Short Break"
-        label="Short Break"
+        label={
+          <div className="flex items-center">
+            <img src={timerIconUrl} alt="Timer" className="mr-2 h-5 w-5" />
+            Short Break
+          </div>
+        }
         value={shortBreak}
         onChange={handleShortBreakChange}
         theme={theme}
         type="number"
-
+        placeholder="Enter Short Break duration"
       />
       <SettingsField
         id="Long Break"
-        label="Long Break"
+        label={
+          <div className="flex items-center">
+            <img src={timerIconUrl} alt="Timer" className="mr-2 h-5 w-5" />
+            Long Break
+          </div>
+        }
         value={longBreak}
         onChange={handleLongBreakChange}
         theme={theme}
         type="number"
-
+        placeholder="Enter Long Break duration"
       />
     </>
-
-  )
+  );
 }
 
-export default TimerField
+export default TimerField;
+
