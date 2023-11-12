f89334a3cb3781475d7c9b7888d8f4ecdd0f362b
neselus <nlu2@njit.edu>
2023-11-11 21:05:18 -0500

    created DateNavigation components and updated HomePage file

---
+++ crushit/web/src/components/DateNavigation/DateNavigation.jsx
@@ -0,0 +1,12 @@
+const DateNavigation = () => {
+  return (
+    <div>
+      <h2>{'DateNavigation'}</h2>
+      <p>
+        {'Find me in ./web/src/components/DateNavigation/DateNavigation.jsx'}
+      </p>
+    </div>
+  )
+}
+
+export default DateNavigation
