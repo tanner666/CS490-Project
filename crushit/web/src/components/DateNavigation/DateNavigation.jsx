e6835345d57c493892ee3a5a675d4b6086b6b0fa
neselus <nlu2@njit.edu>
2023-11-13 00:55:43 -0500

    functionality for next/previous buttons for Date Navigation

--- crushit/web/src/components/DateNavigation/DateNavigation.jsx
+++ crushit/web/src/components/DateNavigation/DateNavigation.jsx
@@ -14,16 +14,42 @@ const DateNavigation = () => {
   let [selectedMonth, setSelectedMonth] = React.useState(today.getMonth() + 1); // Using the month index
   let [selectedYear, setSelectedYear] = React.useState(today.getFullYear());
 
+
   const handleDayChange = (event) => {
-    setSelectedDay(parseInt(event.target.value, 10));
+    const newDay = parseInt(event.target.value, 10);
+    const daysInNewMonth = daysInMonth(selectedYear, selectedMonth);
+
+    // Update the selected day if it's inappropriate for the selected month
+    if (newDay > daysInNewMonth) {
+      setSelectedDay(daysInNewMonth);
+    } else {
+      setSelectedDay(newDay);
+    }
   };
 
   const handleMonthChange = (event) => {
-    setSelectedMonth(parseInt(event.target.value, 10));
+    const newMonth = parseInt(event.target.value, 10);
+    const daysInNewMonth = daysInMonth(selectedYear, newMonth);
+
+    setSelectedMonth(newMonth);
+
+    // Update the selected day if it's greater than the days in the new month
+    if (selectedDay > daysInNewMonth) {
+      setSelectedDay(daysInNewMonth);
+    }
   };
 
   const handleYearChange = (event) => {
-    setSelectedYear(parseInt(event.target.value, 10));
+    const newYear = parseInt(event.target.value, 10);
+    const daysInCurrentMonth = daysInMonth(selectedYear, selectedMonth);
+    const daysInNewMonth = daysInMonth(newYear, selectedMonth);
+
+    setSelectedYear(newYear);
+
+    // Update the selected day if it's greater than the days in the new month
+    if (selectedDay > Math.min(daysInCurrentMonth, daysInNewMonth)) {
+      setSelectedDay(Math.min(daysInCurrentMonth, daysInNewMonth));
+    }
   };
 
   const blueBoxStyle = {
@@ -44,6 +70,75 @@ const DateNavigation = () => {
     alignItems: 'center',
   };
 
+  const navigationButtonStyle = {
+    backgroundColor: 'transparent',
+    border: 'none',
+    outline: 'none',
+    cursor: 'pointer',
+  };
+
+  const handlePrevMonth = () => {
+    setSelectedMonth((prevMonth) => (prevMonth === 1 ? 12 : prevMonth - 1));
+    // Check if the previous day is in the previous month
+    const isPrevDayInPrevMonth = selectedDay === 1;
+    if (isPrevDayInPrevMonth) {
+      handlePrevDay();
+    }
+  };
+
+  const handleNextMonth = () => {
+    setSelectedMonth((prevMonth) => (prevMonth === 12 ? 1 : prevMonth + 1));
+    // Check if the next day is in the next month
+    const isNextDayInNextMonth = selectedDay === daysInMonth(selectedYear, selectedMonth);
+    if (isNextDayInNextMonth) {
+      handleNextDay();
+    }
+  };
+
+  const handlePrevDay = () => {
+      const prevDay = selectedDay - 1;
+
+      // Check if the previous day is in the previous month
+      const isPrevDayInPrevMonth = prevDay === 0;
+      if (isPrevDayInPrevMonth) {
+        const prevMonth = selectedMonth === 1 ? 12 : selectedMonth - 1;
+        const prevYear = selectedMonth === 1 ? selectedYear - 1 : selectedYear;
+        const lastDayOfPrevMonth = daysInMonth(prevYear, prevMonth);
+        setSelectedMonth(prevMonth);
+        setSelectedYear(prevYear);
+        setSelectedDay(lastDayOfPrevMonth);
+      } else {
+        setSelectedDay(prevDay);
+      }
+  };
+
+  const handleNextDay = () => {
+    const nextDay = selectedDay + 1;
+    const daysInCurrentMonth = daysInMonth(selectedYear, selectedMonth);
+
+    // Check if the next day is in the next month
+    const isNextDayInNextMonth = nextDay > daysInCurrentMonth;
+    if (isNextDayInNextMonth) {
+      const nextMonth = selectedMonth === 12 ? 1 : selectedMonth + 1;
+      const nextYear = selectedMonth === 12 ? selectedYear + 1 : selectedYear;
+      setSelectedMonth(nextMonth);
+      setSelectedYear(nextYear);
+      setSelectedDay(1);
+    } else {
+      setSelectedDay(nextDay);
+    }
+  };
+
+  const handlePrevYear = () => {
+    setSelectedYear((prevYear) => prevYear - 1);
+  };
+
+  const handleNextYear = () => {
+    setSelectedYear((prevYear) => prevYear + 1);
+  };
+
+  const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
+
   const roundedBoxStyle = {
     backgroundColor: 'transparent',
     color: theme === 'dark' ? '#FFFFFF' : '#333333',
@@ -54,30 +149,80 @@ const DateNavigation = () => {
     alignItems: 'center',
     position: 'relative',
     border: `1px solid rgba(98, 132, 255, 1)`, // Slightly darker blue border
+    cursor: 'pointer',
   };
 
-  const ImageBox = (imageUrl) => (
-    <div style={roundedBoxStyle}>
+  const ImageBox = (imageUrl, onClick, altText) => (
+    <div style={roundedBoxStyle} onClick={onClick}>
       <img
         src={imageUrl}
-        alt="Arrow"
+        alt={altText}
         style={{ width: '100%', height: '100%', borderRadius: '50%' }}
       />
     </div>
   );
 
-  const DropdownBox = (options, selectedValue, onChange, testId) => (
+  const MonthDropdownBox = ({ selectedValue, onChange }) => (
     <div style={roundedBoxStyle}>
       <select
         className="ml-2"
         style={{ background: 'transparent', border: 'none', outline: 'none' }}
         value={selectedValue}
         onChange={onChange}
-        data-testid={testId} // Assign the data-testid here
+        data-testid="monthDropdown" // Assign the data-testid here
       >
-        {options.map((option, index) => (
-          <option key={index + 1} value={index + 1}>
-            {option}
+        {Array.from({ length: 12 }, (_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {[
+              'January',
+              'February',
+              'March',
+              'April',
+              'May',
+              'June',
+              'July',
+              'August',
+              'September',
+              'October',
+              'November',
+              'December',
+            ][i]}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
+  const DayDropdownBox = ({ selectedValue, onChange }) => (
+    <div style={roundedBoxStyle}>
+      <select
+        className="ml-2"
+        style={{ background: 'transparent', border: 'none', outline: 'none' }}
+        value={selectedValue}
+        onChange={onChange}
+        data-testid="dayDropdown" // Assign the data-testid here
+      >
+        {Array.from({ length: 31 }, (_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
+  const YearDropdownBox = ({ selectedValue, onChange }) => (
+    <div style={roundedBoxStyle}>
+      <select
+        className="ml-2"
+        style={{ background: 'transparent', border: 'none', outline: 'none' }}
+        value={selectedValue}
+        onChange={onChange}
+        data-testid="yearDropdown" // Assign the data-testid here
+      >
+        {Array.from({ length: 100 }, (_, i) => today.getFullYear() - 50 + i).map((year, index) => (
+          <option key={index + 1} value={year}>
+            {year}
           </option>
         ))}
       </select>
@@ -88,33 +233,18 @@ const DateNavigation = () => {
       <div className="w-full p-4 relative">
         <div style={blueBoxStyle}>
           <div style={dateBoxContainerStyle}>
-            {ImageBox("https://drive.google.com/uc?id=1yvUV6NAFgWMWYrNoDOUuHZ92uiHXwyyA")}
-            {DropdownBox(
-              [
-                'January',
-                'February',
-                'March',
-                'April',
-                'May',
-                'June',
-                'July',
-                'August',
-                'September',
-                'October',
-                'November',
-                'December',
-              ],
-              selectedMonth,
-              handleMonthChange,
-              'monthDropdown'
-            )}
-            {ImageBox("https://drive.google.com/uc?id=11WQmToUHDMwNKwAMmdrmgbsXWdPvkJmX")}
-            {ImageBox("https://drive.google.com/uc?id=1yvUV6NAFgWMWYrNoDOUuHZ92uiHXwyyA")}
-            {DropdownBox(Array.from({ length: 31 }, (_, i) => i + 1), selectedDay, handleDayChange, 'dayDropdown')}
-            {ImageBox("https://drive.google.com/uc?id=11WQmToUHDMwNKwAMmdrmgbsXWdPvkJmX")}
-            {ImageBox("https://drive.google.com/uc?id=1yvUV6NAFgWMWYrNoDOUuHZ92uiHXwyyA")}
-            {DropdownBox(Array.from({ length: 50 }, (_, i) => today.getFullYear() - i), selectedYear, handleYearChange, 'yearDropdown')}
-            {ImageBox("https://drive.google.com/uc?id=11WQmToUHDMwNKwAMmdrmgbsXWdPvkJmX")}
+            {ImageBox("https://drive.google.com/uc?id=1yvUV6NAFgWMWYrNoDOUuHZ92uiHXwyyA", handlePrevMonth, 'Previous Month')}
+            {/* Using MonthDropdownBox for the month */}
+            <MonthDropdownBox selectedValue={selectedMonth} onChange={handleMonthChange} />
+            {ImageBox("https://drive.google.com/uc?id=11WQmToUHDMwNKwAMmdrmgbsXWdPvkJmX", handleNextMonth, 'Next Month')}
+            {ImageBox("https://drive.google.com/uc?id=1yvUV6NAFgWMWYrNoDOUuHZ92uiHXwyyA", handlePrevDay, 'Previous Day')}
+            {/* Using DayDropdownBox for the day */}
+            <DayDropdownBox selectedValue={selectedDay} onChange={handleDayChange} />
+            {ImageBox("https://drive.google.com/uc?id=11WQmToUHDMwNKwAMmdrmgbsXWdPvkJmX", handleNextDay, 'Next Day')}
+            {ImageBox("https://drive.google.com/uc?id=1yvUV6NAFgWMWYrNoDOUuHZ92uiHXwyyA", handlePrevYear, 'Previous Year')}
+            {/* Using YearDropdownBox for the year */}
+            <YearDropdownBox selectedValue={selectedYear} onChange={handleYearChange} />
+            {ImageBox("https://drive.google.com/uc?id=11WQmToUHDMwNKwAMmdrmgbsXWdPvkJmX", handleNextYear, 'Next Year')}
           </div>
         </div>
       </div>
@@ -122,4 +252,3 @@ const DateNavigation = () => {
 };
 
 export default DateNavigation;
-
--- crushit/web/src/components/DateNavigation/DateNavigation.test.jsx
+++ crushit/web/src/components/DateNavigation/DateNavigation.test.jsx
@@ -1,7 +1,7 @@
 // web/src/components/DateNavigation/DateNavigation.test.jsx
 
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import DateNavigation from './DateNavigation';
 
 jest.mock('../ThemeContext/ThemeContext', () => ({
@@ -16,12 +16,24 @@ jest.mock('@redwoodjs/router', () => ({
 describe('DateNavigation', () => {
   it('renders successfully', () => {
     render(<DateNavigation />);
-    // Your assertions for rendering go here
+    
+    // Check if the component renders without crashing
+    expect(screen.getByText('Crush It')).toBeInTheDocument();
+    expect(screen.getByAltText('Plan your day illustration')).toBeInTheDocument();
+    expect(screen.getByText('Plan Day')).toBeInTheDocument();
+    expect(screen.getByText('Log Out')).toBeInTheDocument();
   });
 
   it('checks the presence of images', () => {
     render(<DateNavigation />);
-    // Your assertions for images go here
+    
+    // Check if the images are present
+    expect(screen.getByAltText('Previous Month')).toBeInTheDocument();
+    expect(screen.getByAltText('Next Month')).toBeInTheDocument();
+    expect(screen.getByAltText('Previous Day')).toBeInTheDocument();
+    expect(screen.getByAltText('Next Day')).toBeInTheDocument();
+    expect(screen.getByAltText('Previous Year')).toBeInTheDocument();
+    expect(screen.getByAltText('Next Year')).toBeInTheDocument();
   });
 
   it('checks the presence of dropdown boxes', () => {
@@ -37,14 +49,61 @@ describe('DateNavigation', () => {
     expect(yearDropdown).toBeInTheDocument();
   });
 
-  it('checks the functionality of dropdown boxes', () => {
+  it('checks the functionality of previous and next buttons for month, day, and year', () => {
     render(<DateNavigation />);
-    // Get the select elements directly
+    
+    // Simulate user interaction with previous and next buttons
+    fireEvent.click(screen.getByAltText('Previous Month'));
+    fireEvent.click(screen.getByAltText('Next Month'));
+    fireEvent.click(screen.getByAltText('Previous Day'));
+    fireEvent.click(screen.getByAltText('Next Day'));
+    fireEvent.click(screen.getByAltText('Previous Year'));
+    fireEvent.click(screen.getByAltText('Next Year'));
+
+    // Check if the state values are updated correctly
+    // Add your assertions here based on the expected state changes
+
+    // Example assertions for month
+    expect(screen.getByTestId('monthDropdown').value).toBe('11'); // Updated value after clicking next or previous
+
+    // Example assertions for year
+    expect(screen.getByTestId('yearDropdown').value).toBe('2023'); // Updated value after clicking next or previous
+  });
+
+  it('checks the functionality of month dropdown', () => {
+    render(<DateNavigation />);
+    
     const monthDropdown = screen.getByTestId('monthDropdown');
+
+    // Simulate user selecting a month
+    fireEvent.change(monthDropdown, { target: { value: '5' } });
+
+    // Check if the state value is updated correctly
+    expect(screen.getByTestId('monthDropdown').value).toBe('5');
+  });
+
+  it('checks the functionality of day dropdown', () => {
+    render(<DateNavigation />);
+    
     const dayDropdown = screen.getByTestId('dayDropdown');
+
+    // Simulate user selecting a day
+    fireEvent.change(dayDropdown, { target: { value: '20' } });
+
+    // Check if the state value is updated correctly
+    expect(screen.getByTestId('dayDropdown').value).toBe('20');
+  });
+
+  it('checks the functionality of year dropdown', () => {
+    render(<DateNavigation />);
+    
     const yearDropdown = screen.getByTestId('yearDropdown');
 
-    // Your dropdown functionality test assertions go here
+    // Simulate user selecting a year
+    fireEvent.change(yearDropdown, { target: { value: '2023' } });
+
+    // Check if the state value is updated correctly
+    expect(screen.getByTestId('yearDropdown').value).toBe('2023');
   });
 });
 
