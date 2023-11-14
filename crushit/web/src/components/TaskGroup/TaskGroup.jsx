dcfe0a9ab3df1b69cb13b2603ebb70c4be59bba3
Bmogul <98663272+Bmogul@users.noreply.github.com>
2023-11-13 20:49:27 -0500

    Dragging is persistant

--- crushit/web/src/components/TaskGroup/TaskGroup.jsx
+++ crushit/web/src/components/TaskGroup/TaskGroup.jsx
@@ -7,13 +7,13 @@ const TaskGroup = ({groupTitle, tasks, onStatusChange}) => {
   const { theme } = useTheme();
 
   return (
-    <Droppable droppableId={groupTitle}>
+    <Droppable droppableId={groupTitle.replace(/\s+/g, '')}>
     {(provided) =>(
-    <div className={`mb-4 pb-1 min-h-[44px] rounded-lg ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-light-gray text-black'}`}
+    <div className={`mb-4 pb-1 min-h-[80px] rounded-lg ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-light-gray text-black'}` }
     {...provided.droppableProps} ref={provided.innerRef}>
       <h2 className="pl-2 pt-2 text-lg font-dm font-bold text-black">{groupTitle}</h2>
       {tasks.map((task) => (
-        <Draggable key={task.id.toString()} d raggableId={task.id.toString()} index={task.order}>
+        <Draggable key={task.id.toString()} draggableId={task.id.toString()} index={task.order}>
         {(dragProvided) => (
           <div {...dragProvided.draggableProps} {...dragProvided.dragHandleProps} ref={dragProvided.innerRef} >
           <TaskCard task={task} onStatusChange={onStatusChange} />
--- crushit/web/src/components/ToDo/ToDo.jsx
+++ crushit/web/src/components/ToDo/ToDo.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import TaskGroup from '../TaskGroup/TaskGroup';
-import {useQuery} from '@redwoodjs/web';
+import {useQuery, useMutation} from '@redwoodjs/web';
 import AddTaskForm from '../AddTaskForm/AddTaskForm';
 import { DragDropContext } from 'react-beautiful-dnd';
+
 //import GetUserTasksOnDate from 'src/graphql/tasks.gql'
 //import {QUERY} from 'src/graphql/tasks';
 
@@ -71,12 +72,13 @@ const UPDATE_TASK_MUTATION = gql`
 const ToDo = ({userId, day, month, year, formVisibility}) => {
   console.log("UserId in ToDo: ", userId);
   const {data, loading, error, refetch} = useQuery(GetUserTasksOnDate, {variables: {userId, day, month, year}});
-
+  const [updateTasks] = useMutation(UPDATE_TASK_MUTATION);
+  
   const [isFormVisibile, setIsFormVisible] = useState(false);
 
   //define three array groups
   const [tasks, setTasks] = useState({
-    TopPriority: [],
+    'TopPriority': [],
     Important: [],
     Other: [],
   });
@@ -84,7 +86,7 @@ const ToDo = ({userId, day, month, year, formVisibility}) => {
   //function to sort tasks into priority groups
   const sortTasks = (tasks) => {
     const sortedTasks = {
-      TopPriority: [],
+      'TopPriority': [],
       Important: [],
       Other: [],
     };
@@ -93,7 +95,8 @@ const ToDo = ({userId, day, month, year, formVisibility}) => {
       switch(task.ImportanceGroup) {
         case 'TopPriority':
           // task.taskOrder = sortedTasks.TopPriority.length;
-          sortedTasks.TopPriority.push({...task, order: sortedTasks.TopPriority.length});
+          // task.taskOrder = sortedTasks.TopPriority.length;
+          sortedTasks["TopPriority"].push({...task, order: sortedTasks["TopPriority"].length});
           // console.log("TopPriority", sortedTasks.TopPriority);
           break;
         case 'Important':
@@ -115,11 +118,51 @@ const ToDo = ({userId, day, month, year, formVisibility}) => {
 
   // Effect to update tasks when data is fetched
   useEffect(() => {
+    console.log("ToDo useEffect: ");
     if (data && data.userTasksOnDate) {
       const sortedTasks = sortTasks(data.userTasksOnDate);
       setTasks(sortedTasks);
+      console.log(tasks)
     }
   }, [data]);
+
+  useEffect(() => {
+    // Any code that depends on the updated tasks state should go here
+    // Object.keys(tasks).forEach((group) => {
+    //   tasks[group].forEach((task) => {
+    //     if (task.ImportanceGroup !== group) {
+    //       // The task's ImportanceGroup has changed, update it in the database
+    //       const taskId = task.id; // Replace this with the actual ID retrieval for the task
+    //       const newImportanceGroup = task.ImportanceGroup;
+    //       console.log("taskId: ", task, taskId, newImportanceGroup);
+    //       // Call the mutation to update the task's ImportanceGroup in the database
+    //       // You'll need to pass taskId and newImportanceGroup as variables to the mutation
+    //       // You may need to handle errors and loading states appropriately
+    //       // Example:
+    //       // updateTaskMutation({ variables: { id: taskId, input: { ImportanceGroup: newImportanceGroup } } });
+    //     }
+    //   });
+    // });
+    console.log(tasks)
+    Object.keys(tasks).forEach((group) => {
+      // console.log(group)
+      tasks[group].forEach((task) => {
+        // console.log(task)
+        // console.log('data',data)
+        data.userTasksOnDate.filter((task2) =>{ 
+          if(task.id == task2.id){
+            if(task.ImportanceGroup !== task2.ImportanceGroup){
+              updateTasks({variables: {id: task.id, input: {ImportanceGroup: task.ImportanceGroup}}})
+            }
+          }
+        })
+
+      })
+    })
+
+    // console.log(data)
+    
+  }, [tasks]);
 
 
   //need to retrieve info from useState and/or database for this
@@ -146,6 +189,62 @@ const ToDo = ({userId, day, month, year, formVisibility}) => {
   };
 
 
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) {
+      return; // Task dropped outside of a droppable area
+    }
+  
+    // Get the source and destination group IDs
+    const sourceGroupId = result.source.droppableId;
+    const destinationGroupId = result.destination.droppableId;
+  
+    // Copy the tasks object to avoid mutating the state directly
+    const updatedTasks = { ...tasks };
+  
+    // Get the task that was dragged
+    const [draggedTask] = updatedTasks[sourceGroupId].filter((task) => {
+      if(task.id.toString() === result.draggableId)
+        return task
+    }
+      );
+    draggedTask.ImportanceGroup = destinationGroupId;
+    // Remove the task from the source group
+    updatedTasks[sourceGroupId] = updatedTasks[sourceGroupId].filter((task) => task.id.toString() !== result.draggableId);
+  
+    // Determine the new order based on the destination index
+    const newOrder = result.destination.index;
+  
+    // Insert the task into the destination group at the correct order
+    if (destinationGroupId in updatedTasks) {
+      updatedTasks[destinationGroupId].splice(newOrder, 0, {
+        ...draggedTask,
+        order: newOrder,
+      });
+    } else {
+      updatedTasks[destinationGroupId] = [
+        {
+          ...draggedTask,
+          order: newOrder,
+        },
+      ];
+    }
+  
+    // Update the order of all tasks within the source and destination groups
+    // updatedTasks[sourceGroupId] = updatedTasks[sourceGroupId].map((task, index) => ({
+    //   ...task,
+    //   order: index,
+    // }));
+    // if (destinationGroupId in updatedTasks) {
+    //   updatedTasks[destinationGroupId] = updatedTasks[destinationGroupId].map((task, index) => ({
+    //     ...task,
+    //     order: index,
+    //   }));
+    // }
+  
+    // Update the state with the new tasks object
+    setTasks(updatedTasks);
+  }
+
   return (
     <div className="todo-container ">
       {formVisibility && (
@@ -154,10 +253,10 @@ const ToDo = ({userId, day, month, year, formVisibility}) => {
         </div>
       )}
       <div className="p-6 my-2 w-full max-w-[52%] rounded-lg shadow-sm bg-white">
-        <DragDropContext>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
         <TaskGroup
           groupTitle="Top Priority"
-          tasks={tasks.TopPriority}
+          tasks={tasks["TopPriority"]}
           onStatusChange={handleStatusChange}
         />
          <TaskGroup
