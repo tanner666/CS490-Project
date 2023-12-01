02a1e3a4a614aa52840648e53f142a90deb8e87a
KaanIsmet <kaanismet7@gmail.com>
2023-12-01 03:10:06 -0500

    add events for timelines

--- crushit/web/src/components/Appointments/Appointments.jsx
+++ crushit/web/src/components/Appointments/Appointments.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 const Appointments = () => {
   // Example appointment times
   const times = [
@@ -6,19 +8,40 @@ const Appointments = () => {
     '12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM',
   ];
 
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedTime, setSelectedTime] = useState(null);
+
+  const handleTimeClick = (time) => {
+    setSelectedTime(time);
+    setShowPopup(true);
+  };
+
+  const TaskSelectionPopup = () => (
+    <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <div className="bg-white p-4 rounded">
+        <h3 className="font-bold text-lg mb-4">Select Task for {selectedTime}</h3>
+        {/* Placeholder for task selection logic */}
+        <p>Select your task here</p>
+        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={() => setShowPopup(false)}>Close</button>
+      </div>
+    </div>
+  );
+
   return (
     <div>
       <h2 className="text-xl font-bold text-gray-600 mb-3">Appointments</h2>
       <div className="bg-white rounded-lg shadow p-4 overflow-y-auto" style={{ height: "60vh" }}>
         {times.map((time, index) => (
-          <div key={index} className="flex items-center justify-between px-3 py-2 hover:bg-gray-100 cursor-pointer">
-            <span className="text-sm text-gray-700 font-medium">{time}</span>
-            <div className="flex items-center">
-              {/* Placeholder for appointment tasks, if any */}
-            </div>
-          </div>
+          <button
+            key={index}
+            className="flex items-center justify-between w-full px-3 py-2 text-sm text-gray-700 font-medium hover:bg-gray-100"
+            onClick={() => handleTimeClick(time)}
+          >
+            {time}
+          </button>
         ))}
       </div>
+      {showPopup && <TaskSelectionPopup />}
     </div>
   );
 };
