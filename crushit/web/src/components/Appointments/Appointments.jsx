36239bc14fc3b4596f119bef29c724dda3da6836
KaanIsmet <kaanismet7@gmail.com>
2023-12-01 03:05:08 -0500

    Rendering appointments component

--- crushit/web/src/components/Appointments/Appointments.jsx
+++ crushit/web/src/components/Appointments/Appointments.jsx
@@ -6,32 +6,13 @@ const Appointments = () => {
     '12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM',
   ];
 
-  // Styles as variables
-  const containerStyle = {
-    height: "60vh", // Adjust as needed
-  };
-  
-  const appointmentItemStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'between',
-    padding: '12px', // px-3 py-2 in Tailwind
-    borderBottom: '1px solid #E2E8F0', // border-b border-gray-200 in Tailwind
-  };
-
-  const timeStyle = {
-    fontSize: '0.875rem', // text-sm in Tailwind
-    color: '#4B5563', // text-gray-700 in Tailwind
-    fontWeight: 'medium', // font-medium in Tailwind
-  };
-
   return (
-    <div className="bg-white rounded-lg shadow p-4">
+    <div>
       <h2 className="text-xl font-bold text-gray-600 mb-3">Appointments</h2>
-      <div className="overflow-y-auto" style={containerStyle}>
+      <div className="bg-white rounded-lg shadow p-4 overflow-y-auto" style={{ height: "60vh" }}>
         {times.map((time, index) => (
-          <div key={index} style={appointmentItemStyle}>
-            <span style={timeStyle}>{time}</span>
+          <div key={index} className="flex items-center justify-between px-3 py-2 hover:bg-gray-100 cursor-pointer">
+            <span className="text-sm text-gray-700 font-medium">{time}</span>
             <div className="flex items-center">
               {/* Placeholder for appointment tasks, if any */}
             </div>
