2e1b7feae866ddcde3f62bd28486da18c7379451
neselus <nlu2@njit.edu>
2023-12-03 12:37:45 -0500

    created components for displaying times of day

--- crushit/web/src/components/Appointments/Appointments.jsx
+++ crushit/web/src/components/Appointments/Appointments.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Appointments = () => {
   // Example appointment times
   const times = [
@@ -8,38 +10,78 @@ const Appointments = () => {
 
   // Styles as variables
   const containerStyle = {
-    height: "60vh", // Adjust as needed
+    width: '460px',
+    height: '390px',
+    top: '200px',
+    left: '750px',
+    position: 'absolute',
+    borderRadius: '10px',
+    background: 'rgba(255, 255, 255, 1)',
+    boxShadow: '2px 5px 50px 0px rgba(36, 37, 40, 0.1)',
+    padding: '10px',
+    overflow: 'hidden', // Add overflow property to hide overflowing content
   };
-  
+
+  const scrollContainerStyle = {
+    maxHeight: '360px', // Set max height for the scrollable area
+    overflowY: 'auto', // Enable vertical scrolling
+    marginTop: '10px',
+  };
+
   const appointmentItemStyle = {
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'between',
-    padding: '12px', // px-3 py-2 in Tailwind
-    borderBottom: '1px solid #E2E8F0', // border-b border-gray-200 in Tailwind
+    padding: '12px',
+    paddingLeft: '0px',
+    borderBottom: '1px solid #E2E8F0',
   };
 
   const timeStyle = {
-    fontSize: '0.875rem', // text-sm in Tailwind
-    color: '#4B5563', // text-gray-700 in Tailwind
-    fontWeight: 'medium', // font-medium in Tailwind
+    fontFamily: 'DM Sans',
+    fontSize: '14px',
+    fontWeight: 400,
+    lineHeight: '18px',
+    letterSpacing: '0em',
+    textAlign: 'left',
+    color: '#4B5563',
+  };
+
+  const titleStyle = {
+    fontFamily: 'DM Sans',
+    fontSize: '30px',
+    fontWeight: 700,
+    lineHeight: '39px',
+    letterSpacing: '0em',
+    textAlign: 'left',
+    color: '#2D3748',
+    marginBottom: '1rem',
+    position: 'absolute',
+    top: '150px', // Adjust the distance from the top
+    left: '750px',
   };
 
   return (
-    <div className="bg-white rounded-lg shadow p-4">
-      <h2 className="text-xl font-bold text-gray-600 mb-3">Appointments</h2>
-      <div className="overflow-y-auto" style={containerStyle}>
-        {times.map((time, index) => (
-          <div key={index} style={appointmentItemStyle}>
-            <span style={timeStyle}>{time}</span>
-            <div className="flex items-center">
-              {/* Placeholder for appointment tasks, if any */}
-            </div>
+    <>
+      <h2 style={titleStyle}>
+        Appointments
+      </h2>
+      <div style={containerStyle}>
+        <div className="bg-white rounded-lg shadow p-4" style={{ marginTop: '40px', ...scrollContainerStyle }}>
+          <div className="overflow-y-auto">
+            {times.map((time, index) => (
+              <div key={index} style={appointmentItemStyle}>
+                <span style={timeStyle}>{time}</span>
+                <div className="flex items-center">
+                  {/* Placeholder for appointment tasks, if any */}
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export default Appointments;
+
