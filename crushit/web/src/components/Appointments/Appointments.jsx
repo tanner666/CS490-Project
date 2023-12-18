e91987361c58376ff21819238663b8475cc76710
tanner666 <tjg5@njit.edu>
2023-12-18 07:43:18 -0500

    updated styling for appointment events

--- crushit/web/src/components/Appointments/Appointments.jsx
+++ crushit/web/src/components/Appointments/Appointments.jsx
@@ -17,9 +17,11 @@ const GET_EVENTS_QUERY = gql`
   }
 `;
 
-const Appointments = ({start, end, uid}) => {
-  const {theme} = useTheme();
 
+
+const Appointments = ({start, end, uid}) => {
+
+  const {theme} = useTheme();
 
     if (typeof window === "undefined") {
       // If window is undefined, return null or handle appropriately
@@ -56,7 +58,6 @@ const Appointments = ({start, end, uid}) => {
   const times = [
     '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM',
     '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM',
-    '12 AM', '1 AM', '2 AM', '3 AM', '4 AM',
   ];
 
   const [showPopup, setShowPopup] = useState(false);
@@ -111,6 +112,43 @@ const Appointments = ({start, end, uid}) => {
   };
 
   const eventMap = mapEventsToTimes(events);
+  console.log("EventMap: ", eventMap);
+
+  // Function to parse a time string like "5 AM" or "5 PM"
+  const parseTimeString = (timeString, flag) => {
+    const [time, modifier] = timeString.split(' ');
+    let [hours] = time.split(':');
+    hours = parseInt(hours, 10);
+    if (flag){
+      hours+= 1;
+    }
+
+    // Convert to 24-hour time if necessary
+    if (modifier === 'PM' && hours !== 12) {
+      hours += 12;
+    } else if (modifier === 'AM' && hours === 12) {
+      hours = 0; // Midnight is 00:00 in 24-hour time
+    }
+
+    // Create a Date object for today with the specified time
+    const date = new Date();
+    date.setHours(hours, 0, 0, 0); // sets minutes, seconds, and milliseconds to 0
+    return date;
+  };
+
+  function compareTime (s, e) {
+    const currentTime = new Date();
+    const start = new Date(s);
+    const end = new Date(e);
+    console.log("Start: ", start);
+    if (currentTime >= start && currentTime <= end){
+      console.log("Compare time is true: ", start);
+      console.log("Compare time is true: ", end);
+
+      return true;
+    }
+    return false;
+  }
 
   return (
     <div className="ml-1">
@@ -118,18 +156,22 @@ const Appointments = ({start, end, uid}) => {
       <div className={` ${theme === 'dark' ? 'bg-gray-700' : 'bg-white'} rounded-lg shadow p-4 overflow-y-auto scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-gray-100`} style={{ height: "72vh" }}>
         {times.map((time, index) => (
           // Container for the time and task description
-          <div key={index} className="relative px-2 py-2">
-            <span className={`${theme === 'dark' ? 'text-white' : 'text-task-black'} text-sm font-semibold`}>{time}</span>
-            {eventMap[time] && eventMap[time].map((event, eventIndex) => (
-              <div key={eventIndex} className="bg-white absolute top-6 right-2 w-[88%] h-[101%] border-2 border-bar-grey">
-                <button
-                  onClick={() => { setSelectedTask(event.description); handleDescription(event.start, event.summary, event.end); }}
-                  className="text-task-black font-semibold text-sm px-2 py-1 focus:outline-none focus:ring focus:border-blue-300 my-1"
-                >
-                  {event.summary}
-                </button>
-              </div>
-            ))}
+          <div key={index} className="flex flex-col sm:flex-row items-start px-1 py-2">
+            {/**Time label */}
+            <div className={`${compareTime(parseTimeString(time, false), parseTimeString(time, true)) ? 'text-task-blue border-2 p-1 ml-[-6px] mt-[-7px] mb-[-9px] mr-[11px] border-task-blue rounded-md' : (theme === 'dark' ? 'text-white' : 'text-task-black') && 'mr-4'}  text-sm font-semibold`}>{time}</div>
+            {/**Event Container */}
+            <div className="flex-grow">
+              {eventMap[time] && eventMap[time].map((event, eventIndex) => (
+                <div key={eventIndex} className={`${compareTime(event.start, event.end) ? 'bg-transparent-blue' : 'bg-white'} border-2 border-bar-grey ${eventIndex > 0 ? 'mt-0' : 'mt-2'} ${eventIndex == eventMap[time].length-1 ? 'mb-[-25px]' :'mb-0'}`}>
+                  <button
+                    onClick={() => { setSelectedTask(event.description); handleDescription(event.start, event.summary, event.end); }}
+                    className="text-task-black font-semibold text-sm px-2 py-1 focus:outline-none focus:ring focus:border-blue-300 my-1"
+                  >
+                    {event.summary}
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
