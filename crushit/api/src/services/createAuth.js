0939df3e1f61a4db69686a9f409527027b4aa353
tanner666 <tjg5@njit.edu>
2023-12-18 21:13:29 -0500

    testing

--- crushit/api/src/services/createAuth.js
+++ crushit/api/src/services/createAuth.js
@@ -4,6 +4,8 @@ export const getAuthorizationURL = async () => {
   const authURL = process.env.REACT_APP_AUTH_URL;
   const response = await fetch(authURL);
   const json = await response.json();
+  console.log("URL ENV Var: ", process.env.REACT_APP_AUTH_URL);
+  console.log("URL return: ", json.data);
   return {
     url: json.data,
   };
